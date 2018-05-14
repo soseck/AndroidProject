47a5e92fe420e0ceb832049a4856c0257465a49b
Ndeye Sokhna Seck <ndeye-sokhna.seck@etu.unice.fr>
2018-05-14 15:37:12 +0200

    changes with search fragment

--- PolyIncidents/app/src/main/java/fr/unice/polytech/polyincidents/MenuActivity.java
+++ PolyIncidents/app/src/main/java/fr/unice/polytech/polyincidents/MenuActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -67,5 +68,10 @@ public class MenuActivity extends AppCompatActivity {
 
     public BottomNavigationView getBottomNavigationView() {
         return bottomNavigationView;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       return new FragmentSearch().onCreateOptionsMenu(menu);
     }
 }
